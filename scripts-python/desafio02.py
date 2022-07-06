e780391bd300b90bd173c76ec625ae745ec18ff9
Matheus Duarte <107056365+maxthexus@users.noreply.github.com>
2022-07-06 16:52:20 -0300

    update

---
+++ scripts-python/desafio02.py
@@ -0,0 +1,6 @@
+print('Olá quero saber qual sua data de nascimento')
+dia = input('Primeiro me informe o dia ')
+mes = input('agora o mês ')
+ano = input('por fim o ano ')
+
+print('você nasceu no dia ,',dia,' de ',mes, ' de ',ano,' Correto?')
---
+++ scripts-python/desafio03.py
@@ -0,0 +1,4 @@
+num1=input('digite um numero ')
+num2=input('digite um segundo numero ')
+
+print('a soma é ',int(num1)+int(num2))
